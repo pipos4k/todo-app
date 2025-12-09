d0f095c940b09707f972b2053479c709a52c8cf8
pipos4k <kgpapadopoulos@proton.me>
2025-12-09 18:59:12 +0200

    Some fixes, also change POST method (remove redirect-html).

--- todo-app.py
+++ todo-app.py
@@ -17,37 +17,40 @@ def get_all_items():
     items, error = todo_service.get_todo_by_status_filter(status=status)
 
     if error:
-        items = todo_service.get_todo_by_status_filter()
-        return jsonify({"success": True, "items": items}), 200
+        # items = todo_service.get_todo_by_status_filter()
+        return jsonify({"success": True, "items": items}), 400
     
     return jsonify({"success": True, "items": items}), 200
 
 @application.route("/items/<item_id>", methods=["GET"])
 def search_item(item_id):
     item, error = todo_service.get_todo_by_id(item_id=item_id)
+
     if error:
         return jsonify({"success": False, "error": error}), 404
     return jsonify({"success": True, "item": item}), 200
  
-@application.route("/post_item", methods=["GET", "POST"])
+@application.route("/post_item", methods=["POST"])
 def create_item():
-    if request.method == "GET":
-        return render_template("post_item.html")
+    data = request.get_json()
 
-    title = request.form.get("title")
-    description = request.form.get("description")
-    status = request.form.get("status")
+    if not data:
+        return jsonify({"success": False, "error": "No data provided."}), 400
+
+    title = data.get("title")
+    description = data.get("description")
+    status = data.get("status")
 
     created_item, error = todo_service.create_todo_item(
-        title=title,
-        description=description,
-        status=status,
+        title = title,
+        description = description,
+        status = status,
         ) 
     
     if error:
         return jsonify({"success": False, "error": error}), 404
 
-    return redirect(url_for("create_item"))
+    return jsonify({"success": True, "item": created_item}), 201
 
 if __name__ == "__main__":
     application.run(debug=True, host="0.0.0.0", port=5000)
